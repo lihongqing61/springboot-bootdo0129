fc7ec2b70cd16971482226e029a6e104b73acbef
lihongqing1 <li_hongqing1@hoperun.com>
2019-01-29 20:01:31 +0800

    初始化项目

---
+++ src/main/java/com/bootdo/SpringbootBootdo0129Application.java
@@ -0,0 +1,14 @@
+package com.bootdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBootdo0129Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBootdo0129Application.class, args);
+	}
+
+}
+
