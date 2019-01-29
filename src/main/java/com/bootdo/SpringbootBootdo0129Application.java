4d8e2a5f2c84de594172ec835e4cb9727b6103d9
lihongqing1 <li_hongqing1@hoperun.com>
2019-01-29 20:15:26 +0800

    1. 新增静态资源 2. banner.txt

--- src/main/java/com/bootdo/SpringbootBootdo0129Application.java
+++ src/main/java/com/bootdo/SpringbootBootdo0129Application.java
@@ -1,13 +1,22 @@
 package com.bootdo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringbootBootdo0129Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBootdo0129Application.class, args);
+		log.info("ヾ(◍°∇°◍)ﾉﾞ    bootdo启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
+				" ______                    _   ______            \n" +
+				"|_   _ \\                  / |_|_   _ `.          \n" +
+				"  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+				"  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
+				" _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+				"|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
 	}
 
 }
